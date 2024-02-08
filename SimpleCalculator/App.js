1d6774c9b61559b931ec97018892a6a7186ff144
lottasep <seppala.lotta@gmail.com>
2024-02-08 15:28:44 +0200

    Complete SimpleCalculator assignment following the instruction video

--- SimpleCalculator/App.js
+++ SimpleCalculator/App.js
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput, Alert } from "react-native";
 
 export default function App() {
+  const [numA, setNumA] = useState("");
+  const [numB, setNumB] = useState("");
+  const [result, setResult] = useState(0);
+
+  const calculate = (operator) => {
+    if (isNaN(parseFloat(numA)) || isNaN(parseFloat(numB))) {
+      Alert.alert("Warning", "Type a number first");
+    } else {
+      if
+        (operator === "+") setResult(parseFloat(numA) + parseFloat(numB));
+      else
+        setResult(parseFloat(numA) - parseFloat(numB));
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ fontSize: 20 }}>Result: {result.toFixed(2)}</Text>
+      <TextInput
+        keyboardType='numeric'
+        style={{
+          fontSize: 18,
+          width: 200,
+          borderColor: "gray",
+          borderWidth: 1,
+        }}
+        onChangeText={(numA) => setNumA(numA)}
+        value={numA}
+      />
+      <TextInput
+        keyboardType='numeric'
+        style={{
+          fontSize: 18,
+          width: 200,
+          borderColor: "gray",
+          borderWidth: 1,
+        }}
+        onChangeText={(numB) => setNumB(numB)}
+        value={numB}
+      />
+      <View style={styles.buttoncontainer}>
+        <Button onPress={() => calculate("+")} title=' + ' />
+        <Button onPress={() => calculate("-")} title=' - ' />
+      </View>
     </View>
   );
 }
@@ -13,8 +53,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 70,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttoncontainer: {
+    flex: 2,
+    width: 150,
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    alignItems: "flex-start",
+    justifyContent: "space-around",
+    padding: 20,
   },
 });
