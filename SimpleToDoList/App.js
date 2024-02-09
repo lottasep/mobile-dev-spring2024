601ede8cc9675aa7a411f3c49c87281eff16aba1
lottasep <seppala.lotta@gmail.com>
2024-02-09 09:30:38 +0200

    Add basic functionality and styling
    Followed the instruction video from the course and implemented the following features:
    - Ability to add tasks to the to-do list
    - Displaying added tasks in a FlatList
    - Styling the header and input field
    - Adding an item separator to the list

--- SimpleToDoList/App.js
+++ SimpleToDoList/App.js
@@ -1,11 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  FlatList,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleAddTask = () => {
+    // lisätään syötetty data taulukkoon, viimeksi syötetty tallentuu taulukon loppuun
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
+  const itemSeparator = () => {
+    return <View style={{ backgroundColor: "black", height: 1 }}></View>;
+  };
+
+  console.log(tasks);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={{ flex: 1 }}>
+        <Text style={styles.header}>To Do List</Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Write task here'
+          onChangeText={(text) => setTask(text)}
+          value={task}
+        />
+        <Button title='Add Task' onPress={handleAddTask} color='red' />
+      </View>
+      <View style={{ flex: 7 }}>
+        <FlatList
+          data={tasks}
+          ItemSeparatorComponent={itemSeparator}
+          renderItem={({ item }) => (
+            <View style={styles.listItem}>
+              <Text>To do: </Text>
+              <Text>{item}</Text>
+            </View>
+          )}
+        />
+      </View>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -13,8 +57,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    marginTop: 100,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    width: 250,
+    borderColor: "lightgray",
+    padding: 5,
+    borderWidth: 1,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  listItem: {
+    flexDirection: "row",
+    backgroundColor: "lightblue",
+    padding: 5,
+    width: 250,
   },
 });
