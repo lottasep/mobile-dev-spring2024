a540638df1d262b6224c8098306e43e4f753e93c
lottasep <seppala.lotta@gmail.com>
2024-02-16 11:02:28 +0200

    Add bottom view to display additional content
    - Added an another view to the App to display content below the map.
    - The view is initially populated with a placeholder text.

--- LocationApp/App.js
+++ LocationApp/App.js
@@ -1,51 +1,60 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, View, Alert, Text, TextInput, Button } from "react-native";
 import MapView, { Marker } from "react-native-maps";
-import * as Location from 'expo-location';
+import * as Location from "expo-location";
 
 export default function App() {
-    const [location, setLocation] = useState(null);
-    const [errorMsg, setErrorMsg] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-    useEffect(() => {
-        (async () => {
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== 'granted') {
-                setErrorMsg("Permission to access location was denied. \nPlease go to your device's settings and grant permission.");
-                Alert.alert('No permission to get location');
-                return;
-            }
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg(
+          "Permission to access location was denied. \nPlease go to your device's settings and grant permission."
+        );
+        Alert.alert("No permission to get location");
+        return;
+      }
 
-            let location = await Location.getCurrentPositionAsync({});
-            setLocation(location);
-        })();
-    }, []);
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
 
   return (
     <View style={styles.container}>
-        {location ? (
-      <MapView
-        style={styles.mapstyle}
-        initialRegion={{
-          latitude: location.coords.latitude,
-          longitude: location.coords.longitude,
-          latitudeDelta: 0.0322,
-          longitudeDelta: 0.0221,
-        }}
-      >
-        <Marker
-        title="Your Location"
-          coordinate={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-          }}
-        />
-      </MapView>
-         ) : (   
-            <View style={styles.centered}>
-            <Text>{errorMsg || "Loading..."}</Text>
+      {location ? (
+        <>
+          <View style={{ flex: 1 }}>
+            <MapView
+              style={styles.mapstyle}
+              initialRegion={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.0322,
+                longitudeDelta: 0.0221,
+              }}
+            >
+              <Marker
+                title='Your Location'
+                coordinate={{
+                  latitude: location.coords.latitude,
+                  longitude: location.coords.longitude,
+                }}
+              />
+            </MapView>
+            <View style={styles.bottomView}>
+              <Text>This is the bottom view</Text>
+            </View>
           </View>
-         )}
+        </>
+      ) : (
+        <View style={styles.centered}>
+          <Text>{errorMsg || "Loading..."}</Text>
+        </View>
+      )}
     </View>
   );
 }
@@ -55,11 +64,15 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   mapstyle: {
-    flex: 1,
+    flex: 4,
   },
   centered: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  }
+  },
+  bottomView: {
+    flex: 1,
+    padding: 20,
+  },
 });
